f5c723ca34795224bea0a8398e0f1a77c9d35a8d
Timur Lavrentsov <Timur Lavrentsov>
2020-05-30 18:49:24 +0300

    Advanced search(adaptive)

--- searchPlus.php
+++ searchPlus.php
@@ -8,6 +8,8 @@
 
     <link rel="stylesheet" href="styles/styles.css">
     <link rel="stylesheet" type="text/css" href="styles/header-styles.css">
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 </head>
 <body>
 	<div class="wrapper">
